b23bbfcf338a3cdd3af068ea9b2d86970b17c4ec
JeferssonTrejos <jeffersonbloadimirlovo@gmail.com>
2025-05-03 09:23:34 -0600

    update logo site

--- Project_with_vue/src/components/Header.vue
+++ Project_with_vue/src/components/Header.vue
@@ -10,10 +10,6 @@ const isMenuOpen = ref(false);
 function toggleMenu() {
   isMenuOpen.value = !isMenuOpen.value;
 }
-
-function closeMenu() {
-  isMenuOpen.value = false;
-}
 </script>
 
 <template>
