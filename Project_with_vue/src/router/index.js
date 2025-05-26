c5d13730daea6367b35793ba65a65fe675cc3186
jeferssonTrejos <jeffersonbladimirlovo@gmail.com>
2025-05-26 11:35:05 -0600

    fix

--- Project_with_vue/src/router/index.js
+++ Project_with_vue/src/router/index.js
@@ -1,41 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Products from '../views/Products.vue'
-import Order from '../views/Order.vue'
-import Contact from '../views/Contact.vue'
-import NotFound from '../views/NotFound.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Products from "../views/Products.vue";
+import Order from "../views/Order.vue";
+import Contact from "../views/Contact.vue";
+import NotFound from "../views/NotFound.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/products',
-        name: 'Products',
-        component: Products
-    },
-    {
-        path: '/contact',
-        name: 'Contact',
-        component: Contact
-    },
-    {
-        path: '/order',
-        name: 'Order',
-        component: Order
-    },
-    {
-        path: '/:pathMatch(.*)*',
-        name: 'NotFound',
-        component: NotFound
-    }
-]
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/products",
+    name: "Products",
+    component: Products,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
+  },
+  {
+    path: "/order",
+    name: "Order",
+    component: Order,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: NotFound,
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes
-})
+  history: createWebHistory("/"),
+  routes,
+});
 
-export default router
+export default router;
