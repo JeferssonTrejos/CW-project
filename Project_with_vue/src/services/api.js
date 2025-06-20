f83cfed89990e53126fd7f52e2903226415d3fe7
jeferssonTrejos <jeffersonbladimirlovo@gmail.com>
2025-06-20 10:36:17 -0600

    Descomenta la URL de la API de producción y comenta la de localhost para facilitar el despliegue.

--- Project_with_vue/src/services/api.js
+++ Project_with_vue/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const API_URL = "https://cw-project-dl2r.onrender.com/";
+const API_URL = "https://cw-project-dl2r.onrender.com";
 
-const API_URL = "http://localhost:3000";
+// const API_URL = "http://localhost:3000";
 
 const api = axios.create({
   baseURL: API_URL,
